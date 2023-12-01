72638f667fef7f10592b4111ffb75b473d6ca039
Andres7283 <agarcia7283@ceuandalucia.es>
2023-12-01 19:52:55 +0100

    Soy Andrés Avelino García Romero y me encantan los turrones.

---
+++ ProyectoExEj2/src/turron/Turron.java
@@ -0,0 +1,10 @@
+package turron;
+
+public class Turron {
+
+	public static void main(String[] args) {
+
+		System.out.println("Soy Andrés Avelino García Romero y me encantan los turrones.");
+	}
+
+}
