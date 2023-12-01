22fabcfdea046fffe3071d9ad698d4b45931493a
Andres7283 <agarcia7283@ceuandalucia.es>
2023-12-01 19:48:53 +0100

    Soy Andrés Avelino García Romero y me encantan los polvorones.

---
+++ ProyectoExEj2/src/polvoron/Polvoron.java
@@ -0,0 +1,10 @@
+package polvoron;
+
+public class Polvoron {
+
+	public static void main(String[] args) {
+
+		System.out.println("Soy Andrés Avelino García Romero y me encantan los polvorones.");
+	}
+
+}
