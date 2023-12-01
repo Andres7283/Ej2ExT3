dabf9b211f97153e19bd7bb1c5568d1f4e9e5a9b
Andres7283 <agarcia7283@ceuandalucia.es>
2023-12-01 20:00:37 +0100

    Subiendo al repositorio remoto.

--- ProyectoExEj2/src/turron/Turron.java
+++ ProyectoExEj2/src/turron/Turron.java
@@ -4,7 +4,7 @@ public class Turron {
 
 	public static void main(String[] args) {
 
-		System.out.println("Soy Andrés Avelino García Romero y me encantan los turrones.");
+		System.out.println("Soy Andrés Avelino García Romero y me encantan los turrones..");
 	}
 
 }
